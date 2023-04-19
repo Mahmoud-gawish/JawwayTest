7400dd2354ccbf5dff1713b1a31253e6514f05ba
mahmoudgawish <mahmoud.gawish@vodafone.com>
2023-04-19 10:48:05 +0200

    first commit

---
+++ src/main/java/settings/WebsiteSettings.java
@@ -0,0 +1,8 @@
+package settings;
+
+public class WebsiteSettings {
+
+    public static final String browserName = "chrome";
+    public static final String URL = "https://subscribe.jawwy.tv/eg-ar?";
+    public static final String pricingJsonFile = "src/test/java/resources/pricingData.json";
+}
